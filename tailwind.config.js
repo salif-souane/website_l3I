8587e0072165f5e1141bfea44ce423fe8c5a561f
salif-souane <salifsouane1234@gmail.com>
2025-11-06 12:39:51 +0100

    upd conf

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,17 +20,16 @@ export default {
           900: '#153d2a',
   },
         secondary: {
-          50: '#f8fafc',
-          100: '#f1f5f9',
-          200: '#e2e8f0',
-          300: '#cbd5e1',
-          400: '#94a3b8',
-          500: '#64748b',
-          600: '#475569',
-          700: '#334155',
-          800: '#1e293b',
-          900: '#0f172a',
-        }
+          50:  '#f1faf5',
+        100: '#def3e5',
+        200: '#bde8cc',
+        300: '#8dd9aa',
+        400: '#57c482',
+        500: '#3aa76b', // vert secondaire un peu plus clair
+        600: '#2e8f5a',
+        700: '#257349',
+        800: '#1d5c3a',
+        900: '#174930',}
       },
       fontFamily: {
         sans: ['Inter', 'system-ui', 'sans-serif'],
